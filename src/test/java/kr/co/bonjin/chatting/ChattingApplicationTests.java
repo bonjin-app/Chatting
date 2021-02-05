14bdd93f9215c21bdf99baab09659e1c9acc7915
bonjin-all <bonjin.app@gmail.com>
2021-02-05 11:00:05 +0900

    Initial commit

---
+++ src/test/java/kr/co/bonjin/chatting/ChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.bonjin.chatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
